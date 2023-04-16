17d4e090817ba2396c9e82695dde9134fdfe94b3
mk-pmb <mk-pmb@users.noreply.github.com>
2023-04-16 05:58:49 +0200

    v1.0.2: Add tests for type and length.

---
+++ test/all.js
@@ -0,0 +1,6 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+require('./basics.js');
+
+console.info('+OK all tests passed.');
---
+++ test/basics.js
@@ -0,0 +1,10 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+const assert = require('assert');
+const pwhash = require('..');
+
+assert.equal(typeof pwhash, 'string');
+assert.equal(pwhash.length, 98);
+
+console.info('+OK basics tests passed.');
